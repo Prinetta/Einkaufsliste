42d46ca5895635515547e96256d330c43039e24b
Deborah Gäb <debbygaeb@hotmail.de>
2022-11-03 15:52:54 +0100

    Farben hinzugefügt
    Icons hochgeladen, Farben eingestellt erste

--- gelin/app/src/main/java/de/mocows/gelin/MainActivity.kt
+++ gelin/app/src/main/java/de/mocows/gelin/MainActivity.kt
@@ -3,13 +3,20 @@ package de.mocows.gelin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.mocows.gelin.ui.theme.GelinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +27,24 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
+                    Column() {
+                        TextField()
+                        TextField()
+                    }
 
                 }
             }
         }
     }
 }
-@Composable
-fun login(){
 
+@Composable
+fun TextField() {
+    Column(Modifier.padding(16.dp)) {
+        val textState = remember { mutableStateOf(TextFieldValue()) }
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it }
+        )
+    }
 }
--- gelin/app/src/main/java/de/mocows/gelin/ui/theme/Theme.kt
+++ gelin/app/src/main/java/de/mocows/gelin/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = brightgreen,
+    primaryVariant = white,
+    secondary = darkgreen
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = darkgreen,
+    primaryVariant = black,
+    secondary = brightgreen
 
     /* Other default colors to override
     background = Color.White,
